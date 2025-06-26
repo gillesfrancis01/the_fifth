bebbed0f9902432497bd97b419449ab9ecb2208d
Arnold9025 <79923866+Arnold9025@users.noreply.github.com>
2025-06-26 11:32:52 -0400

    Update getAllEvent.ts

--- src/app/actions/getAllEvent.ts
+++ src/app/actions/getAllEvent.ts
@@ -15,11 +15,11 @@ async function getAllEvents(): Promise<events[] | undefined> {
     // Transformation explicite pour assurer la correspondance avec events[]
     const eventsList: events[] = response.documents.map((doc) => ({
       $id: doc.$id,
-      name: (doc as any).name ?? '',
-      description: (doc as any).description ?? '',
-      date: (doc as any).date ?? '',
-      adresse: (doc as any).adresse ?? '',
-      image: (doc as any).image ?? '',
+      name: (doc).name ?? '',
+      description: (doc).description ?? '',
+      date: (doc).date ?? '',
+      adresse: (doc).adresse ?? '',
+      image: (doc).image ?? '',
       // Ajoute ici d'autres propriétés si ton type events en a
     }))
 
