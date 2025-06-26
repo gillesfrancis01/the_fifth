a9940a96b48a368d2147f621b1bfc243719c97f4
Arnold9025 <79923866+Arnold9025@users.noreply.github.com>
2025-06-26 11:51:57 -0400

    Update NextEvent.tsx

--- src/components/NextEvent.tsx
+++ src/components/NextEvent.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import Image from 'next/image'
 import getNextEvent from '@/app/actions/getNextEvent';
 import { Event } from '@/components/Event'
+import { formatDate, formatHour } from '@/app/actions/dateFormat';
 
 const NextEvent = async () => {
     const nextEvent = await getNextEvent();
+    const date = formatDate(nextEvent?.date)
+    const hour = formatHour(nextEvent?.date)
     if (!nextEvent) {
         return (
           <div className="text-center mt-9">
@@ -21,7 +24,7 @@ const NextEvent = async () => {
       <h2 className='text-main text-center text-2xl font-Josefin'>What we do</h2>
         <Image src="/arrows.svg" className="m-auto " width={300} height={100} alt='arrows'/>
         <h3 className='uppercase text-2xl  font-Josefin text-main font-bold '>Next Event</h3>
-        <Event event={nextEvent} />
+        <Event event={nextEvent} date={date} hour={hour} />
 
     </div>
   )
