4faa51c3bd0846b650efd709e092d0f69858e62b
Arnold9025 <79923866+Arnold9025@users.noreply.github.com>
2025-07-23 20:07:04 -0400

    refactor: split NextEvent client logic

--- src/components/NextEvent.tsx
+++ src/components/NextEvent.tsx
@@ -1,40 +1,7 @@
 import React from 'react'
-import Image from 'next/image'
 import getNextEvent from '@/app/actions/getNextEvent'
-import { Event } from '@/components/Event'
 import { formatDate, formatHour } from '@/app/actions/dateFormat'
-import { useLanguage } from '@/context/LanguageContext'
-
-interface NextEventClientProps {
-  nextEvent: Awaited<ReturnType<typeof getNextEvent>> | null
-  date: string
-  hour: string
-}
-
-const NextEventClient = ({ nextEvent, date, hour }: NextEventClientProps) => {
-  'use client'
-  const { t } = useLanguage()
-
-  if (!nextEvent) {
-    return (
-      <div className="text-center mt-9">
-        <h2 className="text-main text-2xl font-Josefin">{t('whatWeDo')}</h2>
-        <Image src="/arrows.svg" className="m-auto" width={300} height={100} alt="arrows" />
-        <h3 className="uppercase text-2xl font-Josefin text-main font-bold">{t('nextEvent')}</h3>
-        <p>{t('noNextEvent')}</p>
-      </div>
-    )
-  }
-
-  return (
-    <div className="text-center mt-9">
-      <h2 className="text-main text-center text-2xl font-Josefin">{t('whatWeDo')}</h2>
-      <Image src="/arrows.svg" className="m-auto " width={300} height={100} alt="arrows" />
-      <h3 className="uppercase text-2xl  font-Josefin text-main font-bold ">{t('nextEvent')}</h3>
-      <Event event={nextEvent} date={date} hour={hour} />
-    </div>
-  )
-}
+import NextEventClient from './NextEventClient'
 
 const NextEvent = async () => {
   const nextEvent = await getNextEvent()
---
+++ src/components/NextEventClient.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import Image from 'next/image'
+import { Event } from '@/components/Event'
+import { useLanguage } from '@/context/LanguageContext'
+import { events } from '@/types'
+
+interface NextEventClientProps {
+  nextEvent: events | null
+  date: string
+  hour: string
+}
+
+const NextEventClient = ({ nextEvent, date, hour }: NextEventClientProps) => {
+  const { t } = useLanguage()
+
+  if (!nextEvent) {
+    return (
+      <div className='text-center mt-9'>
+        <h2 className='text-main text-2xl font-Josefin'>{t('whatWeDo')}</h2>
+        <Image src='/arrows.svg' className='m-auto' width={300} height={100} alt='arrows' />
+        <h3 className='uppercase text-2xl font-Josefin text-main font-bold'>{t('nextEvent')}</h3>
+        <p>{t('noNextEvent')}</p>
+      </div>
+    )
+  }
+
+  return (
+    <div className='text-center mt-9'>
+      <h2 className='text-main text-center text-2xl font-Josefin'>{t('whatWeDo')}</h2>
+      <Image src='/arrows.svg' className='m-auto ' width={300} height={100} alt='arrows' />
+      <h3 className='uppercase text-2xl  font-Josefin text-main font-bold '>{t('nextEvent')}</h3>
+      <Event event={nextEvent} date={date} hour={hour} />
+    </div>
+  )
+}
+
+export default NextEventClient
