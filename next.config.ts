98d79abb705b2be788766f509b4889bdb81dc273
Arnold9025 <79923866+Arnold9025@users.noreply.github.com>
2025-07-29 14:51:59 -0400

    Add remote pattern for fra cloud

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         protocol:'https',
         hostname: 'cloud.appwrite.io',
         pathname:'**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'fra.cloud.appwrite.io',
+        pathname: '**'
       }
     ]
   }
